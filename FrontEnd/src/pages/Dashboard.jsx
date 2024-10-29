9affd8a8903eb3eb062ac78c964a8059f109b7b1
Pavan-0228 <pavanrasal4@gmail.com>
2024-10-29 20:11:48 +0530

    testing ci/cd pipeline

--- FrontEnd/src/pages/Dashboard.jsx
+++ FrontEnd/src/pages/Dashboard.jsx
@@ -342,7 +342,7 @@ const FinanceDashboard = () => {
                 {/* Logo */}
                 <div className="flex items-center">
                     <h1 className="text-2xl lg:text-3xl font-bold">
-                    PennyTracke</h1>
+                    PennyTracker</h1>
                 </div>
 
                 {/* Date and Limit Controls */}
