c850d3288dbb639a46286555cf12a40133c0a878
Pavan-0228 <pavanrasal4@gmail.com>
2024-10-29 20:00:44 +0530

    test4

--- FrontEnd/src/pages/Dashboard.jsx
+++ FrontEnd/src/pages/Dashboard.jsx
@@ -342,7 +342,7 @@ const FinanceDashboard = () => {
                 {/* Logo */}
                 <div className="flex items-center">
                     <h1 className="text-2xl lg:text-3xl font-bold">
-                    Penny</h1>
+                    Penn</h1>
                 </div>
 
                 {/* Date and Limit Controls */}
