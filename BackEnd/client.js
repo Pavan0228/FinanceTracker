415ef8eb8365013ec19725b865d4623932b565e9
Pavan-0228 <pavanrasal4@gmail.com>
2024-11-03 02:37:24 +0530

    Add Redis caching to expense controller and update dependencies

---
+++ BackEnd/client.js
@@ -0,0 +1,11 @@
+// client.js
+import { createClient } from 'redis';
+
+const client = createClient({
+    url: process.env.REDIS_URL || 'redis://localhost:6379'
+});
+
+client.on('error', err => console.log('Redis Client Error', err));
+await client.connect();
+
+export default client;
