f61f06aadc0dad94f3e096fc2bd6fd4685258587
Sridhar1030 <sridharpillai75@gmail.com>
2024-11-06 00:22:50 +0530

    Name Change

--- FrontEnd/src/pages/Dashboard.jsx
+++ FrontEnd/src/pages/Dashboard.jsx
@@ -348,7 +348,7 @@ const FinanceDashboard = () => {
                 {/* Logo */}
                 <div className="flex items-center">
                     <h1 className="text-2xl lg:text-3xl font-bold">
-                    PennyTracker</h1>
+                    PennyWise</h1>
                 </div>
 
                 {/* Date and Limit Controls */}
